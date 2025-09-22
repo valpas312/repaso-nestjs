ed7540fc1e6e9b28c131024522dcf44b0fbe83c8
valpas312 <valenpalmas2005@gmail.com>
2025-09-22 19:20:31 -0300

    agregando swagger porque no tengo el modulo mapped-types - ver esto

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,20 +1,4 @@
-import { IsString, IsBoolean, IsOptional, MinLength, MaxLength } from 'class-validator';
-import { Task } from "generated/prisma";
+import { PartialType } from '@nestjs/swagger';
+import { CreateTaskDto } from './create-task.dto';
 
-export class UpdateTaskDto implements Partial<Omit<Task, 'id'>> {
-    @IsOptional()
-    @IsString()
-    @MinLength(3)
-    @MaxLength(50)
-    title?: string;
-
-    @IsOptional()
-    @IsString()
-    @MinLength(3)
-    @MaxLength(200)
-    description?: string;
-
-    @IsOptional()
-    @IsBoolean()
-    completed?: boolean;
-}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
