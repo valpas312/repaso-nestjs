1083163bf11386cc39e5b027754433d6ecb95dca
valpas312 <valenpalmas2005@gmail.com>
2025-09-27 20:51:43 -0300

    modificando la configuracion de cors para cualquier origen

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: true,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({
